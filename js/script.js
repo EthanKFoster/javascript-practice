2858d2564ab2460c1259b3156c61d46db82ae30a
EthanKFoster <ethankfoster@gmail.com>
2021-05-25 08:38:23 -0700

    may 25 update; window changes

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+// let
+//const
+
+const car = "ford";
+const ages = [12, 18, 19, 0]
+console.log(car);
+
+const thetype = typeof car;
+
+console.table(ages);
+console.log(typeof car);
+console.log(thetype)
+
+
+const movie = "home alone ";
+console.log(movie);
+
+const phrase = "go away";
+
+console.log(movie + phrase)
+
+console.log("hey baby, I'm " + movie)
+
+const password = "asdasfaweeuwuetueffjzjcvjzxc";
+
+console.log(password.length);
+
+let sardines;
+
+console.log(sardines);
+
+console.log(50*50/89);
+let er = 1;
+console.log(er === er);
+
+console.log(NaN = null)
+
+const decision = window.confirm("are you sure?")
+
+console.log(decision)
